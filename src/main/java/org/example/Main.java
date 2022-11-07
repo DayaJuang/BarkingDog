9f2e775f0d4cf02d09b1ff531f8c7e412b8ec7a0
daya <dayajuangmahaputra@gmail.com>
2022-11-07 19:45:23 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,25 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean isShouldWakeUp = shouldWakeUp(true, -8);
+
+        System.out.println(isShouldWakeUp);
+    }
+
+    public static boolean shouldWakeUp(boolean isDogBark, int hoursOfDay){
+        boolean result = false;
+
+        // Check if the hoursOfDay is valid (between 0-23)
+        if(hoursOfDay < 0 || hoursOfDay > 23){
+            return false;
+        }
+
+        // If dog is barking before 8 and after 22 return true
+        if(isDogBark && (hoursOfDay < 8 || hoursOfDay > 22)){
+            result = true;
+        }
+
+        return result;
     }
 }
